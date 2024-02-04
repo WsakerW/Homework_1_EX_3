c932fd1d0953a200051c0fe0e4565b4e9a5f51e3
WsakerW <mohammad.saker6@gmail.com>
2024-02-05 01:50:24 +0200

    This is ex3. the changes are:
    In Roots.java -> I fixed some bugs such as in line 20 the formula of q
    was b*b -2*a*c
    and instead b*b -4*a*c
    the ranges at line 22 of a and b had to change them to 100 from 50
    from line 27 – 44 there was some mistakes of the enum result of each
    statement
    
    in RootsTest -> I added partitions of each test case with the errors to
    confirm the expected result

--- Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
+++ Root-classificatin/src/test/java/Root_classificatin/Root_classificatin/RootsTest.java
@@ -1,22 +1,81 @@
 package Root_classificatin.Root_classificatin;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class RootsTest 
-{
+public class RootsTest {
 	Roots root;
 
 	@Test
-	public void testPartion1() 
-	{
-		//(0,1,2)
+	public void testPartion1_error1() {
+		// (0,1,2)
 		Roots.calculate_roots(-2, 1, 2);
-		
-		String  root_expected = Root_Types.ERROR.toString();
-		
-		
+
+		String root_expected = Root_Types.ERROR.toString();
+
 		assertEquals(root_expected, Roots.root_types().toString());
 	}
 	
+	@Test
+	public void testPartion1_error2() {
+		
+		Roots.calculate_roots(2, -1, 2);
+
+		String root_expected = Root_Types.ERROR.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
 	}
+	
+	@Test
+	public void testPartion1_error3() {
+		
+		Roots.calculate_roots(2, 1, 101);
+
+		String root_expected = Root_Types.ERROR.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	@Test
+	public void testPartion2_NotQuadratic() {
+		
+		Roots.calculate_roots(0, 5, 20);
+
+		String root_expected = Root_Types.Not_quadratic.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	@Test
+	public void testPartion3_NoRoots() {
+		
+		Roots.calculate_roots(99, 6, 7);
+
+		String root_expected = Root_Types.No_roots.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	@Test
+	public void testPartion4_TwoRoots() {
+		
+		Roots.calculate_roots(20, 5, 0);
+
+		String root_expected = Root_Types.Two_roots.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+	
+	@Test
+	public void testPartion5_EqualRoots() {
+		
+		Roots.calculate_roots(1, 4, 4);
+
+		String root_expected = Root_Types.Equal_roots.toString();
+
+		assertEquals(root_expected, Roots.root_types().toString());
+	}
+
+
+}
